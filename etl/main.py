4aa1027e102439512f5e415e4ae576533d0b90eb
Rochdyath <rochdiath.bachabi@epitech.eu>
2025-12-08 13:43:47 +0100

    Script de stockage des données dans le bucket s3

--- etl/main.py
+++ etl/main.py
@@ -1,22 +1,31 @@
 import os
 from ingest_data import ingest_csv
+from upload_data import upload_to_s3
 
 def main():
-    print("Lancement de l'ETL — étape 1 : ingestion des données")
+    print("Lancement du pipeline ETL")
 
-    # Chemin du fichier à ingérer
+    # Chemin du fichier échantillon
     sample_path = os.path.join("data", "samples", "sample_energy_data.csv")
-
-    # Vérification de l'existence du fichier
     if not os.path.exists(sample_path):
-        print(f"ERREUR : le fichier {sample_path} est introuvable.")
+        print(f"ERREUR : fichier {sample_path} introuvable")
         return
 
-    # Exécution de l’ingestion
+    # ------------------------
+    # Étape 1 : Ingestion
+    # ------------------------
+    print("Étape 1 : Ingestion")
     ingest_csv(sample_path)
 
-    print("\nPipeline ETL terminé ! (Étape 1 Ingestion)")
+    # ------------------------
+    # Étape 2 : Upload S3
+    # ------------------------
+    print("\nÉtape 2 : Stockage dans le Data Lake (AWS S3)")
+    bucket_name = "wai-data"  # Remplacer par ton bucket
+    prefix = "samples"             # Préfixe dans le bucket
+    upload_to_s3(sample_path, bucket_name, prefix=prefix)
 
+    print("\nPipeline ETL terminé !")
 
 if __name__ == "__main__":
     main()
